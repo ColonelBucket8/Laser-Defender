29610607e1aebf25c5bd2d85eeed4466300c0bac
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-21 17:41:40 +0800

    Add boundaries 1. Add viewport to restrict player in a box 2. Add padding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,42 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float paddingLeft;
+    [SerializeField] private float paddingRight;
+    [SerializeField] private float paddingTop;
+    [SerializeField] private float paddingBottom;
+
+    private Vector2 maxBounds;
+    private Vector2 minBounds;
     private Vector2 rawInput;
+
+    private void Start()
+    {
+        InitBounds();
+    }
 
     private void Update()
     {
         Move();
     }
 
+    private void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
     private void Move()
     {
-        Vector3 delta = rawInput * (moveSpeed * Time.deltaTime);
-        transform.position += delta;
+        Vector2 delta = rawInput * (moveSpeed * Time.deltaTime);
+        var newPos = new Vector2
+        {
+            x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight),
+            y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop)
+        };
+
+        transform.position = newPos;
     }
 
     private void OnMove(InputValue value)
